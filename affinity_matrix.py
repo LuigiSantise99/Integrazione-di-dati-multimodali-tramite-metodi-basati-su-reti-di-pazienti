995d96d6ca2eb5615356e5f8196fa767e93626c4
LuigiSantise <l.santise@campus.unimib.it>
2024-09-19 11:30:17 +0200

    il caricamento dei dati methy ora funziona, considerandone solo una parte

--- affinity_matrix.py
+++ affinity_matrix.py
@@ -9,17 +9,17 @@ from sklearn.utils.extmath import randomized_svd
 
 cancer_type = 'BLCA2'
 
-# Carica i dati da un file Excel
-data_type_methy = f"dataset/cancer/{cancer_type}_Methy.csv"
+# Carica i dati
 data_type_mirna = f"dataset/cancer/{cancer_type}_miRNA.csv"
 data_type_rnaseq = f"dataset/cancer/{cancer_type}_RNASeq.csv"
 data_type_rppa = f"dataset/cancer/{cancer_type}_RPPA.csv"
-df_rppa = pd.read_csv(data_type_rppa)
+data_type_methy = f"dataset/cancer/{cancer_type}_Methy.csv"
 df_mirna = pd.read_csv(data_type_mirna)
 df_rnaseq = pd.read_csv(data_type_rnaseq)
-#df_methy = pd.read_csv(data_type_methy, nrows=2) # ci mette molto tempo a caricare il file intero
+df_rppa = pd.read_csv(data_type_rppa)
+df_methy = pd.read_csv(data_type_methy, usecols=range(0, 10000)) # ci mette molto tempo a caricare il file intero
 
-print(df_mirna.shape, df_rnaseq.shape, df_rppa.shape)
+print(df_mirna.shape, df_rnaseq.shape, df_rppa.shape, df_methy.shape)
 
 def calculate_n_components(df, proportion=0.1):
     """
@@ -53,16 +53,16 @@ def apply_rsvd(data_matrix, n_components):
 n_components_mirna = calculate_n_components(df_mirna, proportion=0.2)  # 20% per miRNA
 n_components_rnaseq = calculate_n_components(df_rnaseq, proportion=0.05)  # 5% per RNASeq
 n_components_rppa = calculate_n_components(df_rppa, proportion=0.2)  # 20% per RPPA
-#n_components_methy = calculate_n_components(df_methy, proportion=0.01)  # 1% per Methy
+n_components_methy = calculate_n_components(df_methy, proportion=0.05)  # 5% per Methy
 print(n_components_mirna, n_components_rnaseq, n_components_rppa)
 
 # Applichiamo RSVD su ogni tipo di dato
 df_mirna_reduced = apply_rsvd(df_mirna, n_components_mirna)
 df_rnaseq_reduced = apply_rsvd(df_rnaseq, n_components_rnaseq)
 df_rppa_reduced = apply_rsvd(df_rppa, n_components_rppa)
-# blca_methy_reduced = apply_rsvd(df_methy, n_components_methy)
+df_methy_reduced = apply_rsvd(df_methy, n_components_methy)
 
-print(df_mirna_reduced.shape, df_rnaseq_reduced.shape, df_rppa_reduced.shape)
+print(df_mirna_reduced.shape, df_rnaseq_reduced.shape, df_rppa_reduced.shape, df_methy_reduced.shape)
 
 '''
 # Estrai i dati e le etichette
@@ -85,8 +85,8 @@ affinity_networks_rnaseq = snf.make_affinity(df_rnaseq_reduced, metric='euclidea
 print("Rnaseq data affinity networks created")
 affinity_networks_rppa = snf.make_affinity(df_rppa_reduced, metric='euclidean', K=20, mu=0.5, normalize=True)
 print("Rppa data affinity networks created")
-# affinity_networks_methy = snf.make_affinity(blca_methy_reduced, metric='euclidean', K=20, mu=0.5, normalize=True)
-# print("Methy data affinity networks created")
+affinity_networks_methy = snf.make_affinity(df_methy_reduced, metric='euclidean', K=20, mu=0.5, normalize=True)
+print("Methy data affinity networks created")
 
 directory = f'{cancer_type}'
 if not os.path.exists(directory):
@@ -96,7 +96,7 @@ if not os.path.exists(directory):
 np.save(f'{cancer_type}/fused_affinity_matrix_mirna.npy', affinity_networks_mirna)
 np.save(f'{cancer_type}/fused_affinity_matrix_rnaseq.npy', affinity_networks_rnaseq)
 np.save(f'{cancer_type}/fused_affinity_matrix_rppa.npy', affinity_networks_rppa)
-# np.save(f'{cancer_type}/fused_affinity_matrix_methy.npy', affinity_networks_methy)
+np.save(f'{cancer_type}/fused_affinity_matrix_methy.npy', affinity_networks_methy)
 
 # plt.imshow(affinity_networks_mirna, cmap='viridis', interpolation='nearest')
 # plt.colorbar()
@@ -114,7 +114,7 @@ np.save(f'{cancer_type}/fused_affinity_matrix_rppa.npy', affinity_networks_rppa)
 # plt.savefig(f'{cancer_type}/exp_affinity_matrix_plot.png')  # Salva il plot come immagine
 
 # Calcolo della matrice di affinità fuse
-fused_affinity = snf.snf([affinity_networks_mirna, affinity_networks_rnaseq, affinity_networks_rppa], K=20) #manca affinity_networks_methy
+fused_affinity = snf.snf([affinity_networks_mirna, affinity_networks_rnaseq, affinity_networks_rppa, affinity_networks_methy], K=20)
 print(f"Fused affinity networks created for {cancer_type}")
 
 # Salva la matrice di affinità fusa in npy e png
@@ -124,6 +124,13 @@ plt.imshow(fused_affinity, cmap='viridis', interpolation='nearest')
 plt.colorbar()
 plt.title(f'Fused Affinity Matrix - {cancer_type}')
 plt.savefig(f'{cancer_type}/fused_affinity_matrix_plot.png')  # Salva il plot come immagine
+
+'''
+# Carica la matrice di affinità fusa e la stampa 
+loaded_matrix = np.load(f'{cancer_type}/fused_affinity_matrix.npy')
+print(loaded_matrix.shape)
+print(loaded_matrix[:5])  # Controlla i primi 5 valori
+'''
 
 '''
 # Determinazione del numero ottimale di cluster

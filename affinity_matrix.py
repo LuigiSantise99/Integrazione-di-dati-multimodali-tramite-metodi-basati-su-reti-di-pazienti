6ce96b3213852ac5c897cbfef851dd3820bfe3f7
Luigi Santise <santise99@gmail.com>
2024-12-20 19:02:34 +0100

    snf sui nuovi dati espressione + appunti

--- affinity_matrix.py
+++ affinity_matrix.py
@@ -5,101 +5,100 @@ import os
 import matplotlib.pyplot as plt
 from sklearn.utils.extmath import randomized_svd
 
-cancer_type = 'BLCA2'
+tcga_project = 'BRCA'
 
-# Carica i dati
-data_type_mirna = f"dataset/cancer/{cancer_type}_miRNA.csv"
-data_type_rnaseq = f"dataset/cancer/{cancer_type}_RNASeq.csv"
-data_type_rppa = f"dataset/cancer/{cancer_type}_RPPA.csv"
-#data_type_methy = f"dataset/cancer/{cancer_type}_Methy.csv"
-df_mirna = pd.read_csv(data_type_mirna)
-df_rnaseq = pd.read_csv(data_type_rnaseq)
-df_rppa = pd.read_csv(data_type_rppa)
-#df_methy = pd.read_csv(data_type_methy, usecols=range(0, 10000)) # ci mette molto tempo a caricare il file intero
+# Caricamento dei dati di espressione (datExpr_<omics>)
+datExpr_DNAm = f"../MOGDx/data/TCGA/{tcga_project}/raw/datExpr_DNAm.csv"
+datExpr_miRNA = f"../MOGDx/data/TCGA/{tcga_project}/raw/datExpr_miRNA.csv"
+datExpr_mRNA = f"../MOGDx/data/TCGA/{tcga_project}/raw/datExpr_mRNA.csv"
+
+df_DNAm = pd.read_csv(datExpr_DNAm)
+df_miRNA = pd.read_csv(datExpr_miRNA)
+df_mRNA = pd.read_csv(datExpr_mRNA)
 
 print("Shape prima di RSVD:")
-print(df_mirna.shape, df_rnaseq.shape, df_rppa.shape) #, df_methy.shape)
+print(df_DNAm.shape, df_miRNA.shape, df_mRNA.shape)
 
-def calculate_n_components(df, proportion=1):
-    """
-    Calcola il numero di componenti da mantenere, tenendo conto che n_components non deve superare il numero di righe.
+# def calculate_n_components(df, proportion=1):
+#     """
+#     Calcola il numero di componenti da mantenere, tenendo conto che n_components non deve superare il numero di righe.
     
-    df: Il dataframe con i dati.
-    proportion: La proporzione delle colonne da mantenere.
-    return: Il numero di componenti da usare nell'RSVD.
-    """
-    n_components = min(int(df.shape[0] * proportion), int(df.shape[1] * proportion)) - 1  # Calcola il numero di componenti
-    return n_components
+#     df: Il dataframe con i dati.
+#     proportion: La proporzione delle colonne da mantenere.
+#     return: Il numero di componenti da usare nell'RSVD.
+#     """
+#     n_components = min(int(df.shape[0] * proportion), int(df.shape[1] * proportion)) - 1  # Calcola il numero di componenti
+#     return n_components
 
-def apply_rsvd(data_matrix, n_components):
-    """
-    Applica RSVD su un set di dati.
+# def apply_rsvd(data_matrix, n_components):
+#     """
+#     Applica RSVD su un set di dati.
 
-    data_matrix: DataFrame con dati multi-omici.
-    n_components: Numero di componenti da mantenere dopo la riduzione dimensionale.
-    return: Il dataframe con dimensionalità ridotta. 
-    """
-    # Estrae la matrice dai dati
-    X = np.array(data_matrix.iloc[:, 1:], dtype=np.float64)
+#     data_matrix: DataFrame con dati multi-omici.
+#     n_components: Numero di componenti da mantenere dopo la riduzione dimensionale.
+#     return: Il dataframe con dimensionalità ridotta. 
+#     """
+#     # Estrae la matrice dai dati
+#     X = np.array(data_matrix.iloc[:, 1:], dtype=np.float64)
     
-    # Applica RSVD
-    U, Sigma, VT = randomized_svd(X, n_components=n_components, random_state=42)
+#     # Applica RSVD
+#     U, Sigma, VT = randomized_svd(X, n_components=n_components, random_state=42)
     
-    # Proietta i dati nello spazio ridotto
-    X_reduced = U @ np.diag(Sigma)
+#     # Proietta i dati nello spazio ridotto
+#     X_reduced = U @ np.diag(Sigma)
     
-    # Restituisce il risultato come DataFrame
-    return pd.DataFrame(X_reduced, index=data_matrix.index)
+#     # Restituisce il risultato come DataFrame
+#     return pd.DataFrame(X_reduced, index=data_matrix.index)
 
-n_components_mirna = calculate_n_components(df_mirna, proportion=0.2)  # 20% per miRNA
-n_components_rnaseq = calculate_n_components(df_rnaseq, proportion=0.5)  # 50% per RNASeq
-n_components_rppa = calculate_n_components(df_rppa, proportion=0.2)  # 20% per RPPA
-#n_components_methy = calculate_n_components(df_methy, proportion=0.05)  # 5% per Methy
+# n_components_mirna = calculate_n_components(df_mirna, proportion=0.2)  # 20% per miRNA
+# n_components_rnaseq = calculate_n_components(df_rnaseq, proportion=0.5)  # 50% per RNASeq
+# n_components_rppa = calculate_n_components(df_rppa, proportion=0.2)  # 20% per RPPA
+# #n_components_methy = calculate_n_components(df_methy, proportion=0.05)  # 5% per Methy
 
-# Applichiamo RSVD su ogni tipo di dato
-df_mirna_reduced = apply_rsvd(df_mirna, n_components_mirna)
-df_rnaseq_reduced = apply_rsvd(df_rnaseq, n_components_rnaseq)
-df_rppa_reduced = apply_rsvd(df_rppa, n_components_rppa)
-#df_methy_reduced = apply_rsvd(df_methy, n_components_methy)
+# # Applichiamo RSVD su ogni tipo di dato
+# df_mirna_reduced = apply_rsvd(df_mirna, n_components_mirna)
+# df_rnaseq_reduced = apply_rsvd(df_rnaseq, n_components_rnaseq)
+# df_rppa_reduced = apply_rsvd(df_rppa, n_components_rppa)
+# #df_methy_reduced = apply_rsvd(df_methy, n_components_methy)
 
-print("Shape dopo RSVD:")
-print(df_mirna_reduced.shape, df_rnaseq_reduced.shape, df_rppa_reduced.shape) #, df_methy_reduced.shape)
+# print("Shape dopo RSVD:")
+# print(df_mirna_reduced.shape, df_rnaseq_reduced.shape, df_rppa_reduced.shape) #, df_methy_reduced.shape)
 
-# Calcolo delle matrici di affinità
-affinity_networks_mirna = snf.make_affinity(df_mirna_reduced, metric='euclidean', K=20, mu=0.5, normalize=True)
-print("Mirna data affinity networks created")
-affinity_networks_rnaseq = snf.make_affinity(df_rnaseq_reduced, metric='euclidean', K=20, mu=0.5, normalize=True)
-print("Rnaseq data affinity networks created")
-affinity_networks_rppa = snf.make_affinity(df_rppa_reduced, metric='euclidean', K=20, mu=0.5, normalize=True)
-print("Rppa data affinity networks created")
-#affinity_networks_methy = snf.make_affinity(df_methy_reduced, metric='euclidean', K=20, mu=0.5, normalize=True)
-#print("Methy data affinity networks created")
+# # Calcolo delle matrici di affinità
+# affinity_networks_mirna = snf.make_affinity(df_mirna_reduced, metric='euclidean', K=20, mu=0.5, normalize=True)
+# print("Mirna data affinity networks created")
+# affinity_networks_rnaseq = snf.make_affinity(df_rnaseq_reduced, metric='euclidean', K=20, mu=0.5, normalize=True)
+# print("Rnaseq data affinity networks created")
+# affinity_networks_rppa = snf.make_affinity(df_rppa_reduced, metric='euclidean', K=20, mu=0.5, normalize=True)
+# print("Rppa data affinity networks created")
+# #affinity_networks_methy = snf.make_affinity(df_methy_reduced, metric='euclidean', K=20, mu=0.5, normalize=True)
+# #print("Methy data affinity networks created")
 
-directory = f'{cancer_type}'
-if not os.path.exists(directory):
-    os.makedirs(directory)
+# directory = f'{cancer_type}'
+# if not os.path.exists(directory):
+#     os.makedirs(directory)
 
-# Salva le matrici di affinità in npy e png
-np.save(f'{cancer_type}/fused_affinity_matrix_mirna.npy', affinity_networks_mirna)
-np.save(f'{cancer_type}/fused_affinity_matrix_rnaseq.npy', affinity_networks_rnaseq)
-np.save(f'{cancer_type}/fused_affinity_matrix_rppa.npy', affinity_networks_rppa)
-#np.save(f'{cancer_type}/fused_affinity_matrix_methy.npy', affinity_networks_methy)
+# # Salva le matrici di affinità in npy e png
+# np.save(f'{cancer_type}/fused_affinity_matrix_mirna.npy', affinity_networks_mirna)
+# np.save(f'{cancer_type}/fused_affinity_matrix_rnaseq.npy', affinity_networks_rnaseq)
+# np.save(f'{cancer_type}/fused_affinity_matrix_rppa.npy', affinity_networks_rppa)
+# #np.save(f'{cancer_type}/fused_affinity_matrix_methy.npy', affinity_networks_methy)
 
-# Calcolo della matrice di affinità fuse
-fused_affinity = snf.snf([affinity_networks_mirna, affinity_networks_rnaseq, affinity_networks_rppa], K=20) #, affinity_networks_methy]
-print(f"Fused affinity networks created for {cancer_type}")
+# # Calcolo della matrice di affinità fuse
+# fused_affinity = snf.snf([affinity_networks_mirna, affinity_networks_rnaseq, affinity_networks_rppa], K=20) #, affinity_networks_methy]
+# print(f"Fused affinity networks created for {cancer_type}")
 
-# Salva la matrice di affinità fusa in npy e png
-np.save(f'{cancer_type}/fused_affinity_matrix.npy', fused_affinity)
+# # Salva la matrice di affinità fusa in npy e png
+# np.save(f'{cancer_type}/fused_affinity_matrix.npy', fused_affinity)
 
-plt.imshow(fused_affinity, cmap='viridis', interpolation='nearest')
-plt.colorbar()
-plt.title(f'Fused Affinity Matrix - {cancer_type}')
-plt.savefig(f'{cancer_type}/fused_affinity_matrix_plot.png')  # Salva il plot come immagine
+# plt.imshow(fused_affinity, cmap='viridis', interpolation='nearest')
+# plt.colorbar()
+# plt.title(f'Fused Affinity Matrix - {cancer_type}')
+# plt.savefig(f'{cancer_type}/fused_affinity_matrix_plot.png')  # Salva il plot come immagine
 
-'''
-# Carica la matrice di affinità fusa e la stampa 
-loaded_matrix = np.load(f'{cancer_type}/fused_affinity_matrix.npy')
-print(loaded_matrix.shape)
-print(loaded_matrix[:5])  # Controlla i primi 5 valori
-'''
+# '''
+# # Carica la matrice di affinità fusa e la stampa 
+# loaded_matrix = np.load(f'{cancer_type}/fused_affinity_matrix.npy')
+# print(loaded_matrix.shape)
+# print(loaded_matrix[:5])  # Controlla i primi 5 valori
+# '''
